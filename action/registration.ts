82e1772acf1f076be20a388380b7a21fcbf25de4
kapepa <kapepayx@gmail.com>
2024-01-23 22:54:59 +0200

    Registration

--- action/registration.ts
+++ action/registration.ts
@@ -1,5 +1,10 @@
+"use server"
+
 import { RegistrationSchema } from "@/schemas"
 import * as z from "zod"
+import bcrypt from "bcrypt"
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 const Registration = async (values: z.infer<typeof RegistrationSchema>) => {
   const result = RegistrationSchema.safeParse(values);
@@ -7,7 +12,16 @@ const Registration = async (values: z.infer<typeof RegistrationSchema>) => {
   if (!result.success) {
     return {error: "Invalid fields!"}
   } else {
-    return {success: "Registration is success"}
+    const { name, email, password } = result.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existUser = await getUserByEmail(email);
+
+    if(existUser) return {error: "Email alredy is use!"};
+
+    await db.user.create({ data: { name, email, password: hashedPassword } })
+    
+    return {success: "User created!"};
   }
 }
 
