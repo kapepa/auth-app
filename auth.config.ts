e7e1f33e96d3edc5d25e299ceac1566f4d179541
kapepa <kapepayx@gmail.com>
2024-02-03 23:57:04 +0200

    auth.config

--- auth.config.ts
+++ auth.config.ts
@@ -1,7 +1,40 @@
-import GitHub from "next-auth/providers/github"
+import CredentialsProvider from "next-auth/providers/credentials";
 
 import type { NextAuthConfig } from "next-auth"
+import { LoginSchema } from "./schemas";
+import { getUserByEmail } from "./data/user";
+import bcrypt from "bcrypt"
 
 export default {
-  providers: [GitHub],
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: {  label: "Password", type: "password" }
+      },
+      async authorize(credentials, req) {
+        const validateFields = LoginSchema.safeParse(credentials);
+  
+        if (validateFields.success) {
+          // Any object returned will be saved in `user` property of the JWT
+          const { email, password } = validateFields.data;
+          const user = await getUserByEmail(email);
+
+          if(!user || !user.password) return null;
+
+          const passwordMatch = await bcrypt.compare(password, user.password);
+
+          if(passwordMatch) return user;
+
+          return null;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null
+  
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      }
+    })
+  ],
 } satisfies NextAuthConfig
--- auth.ts
+++ auth.ts
@@ -3,7 +3,12 @@ import { PrismaAdapter } from "@auth/prisma-adapter"
 import authConfig from "./auth.config"
 import { db } from "./lib/db";
 
-export const { handlers, auth } = NextAuth({
+export const { 
+  handlers, 
+  auth, 
+  signIn, 
+  signOut 
+} = NextAuth({
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
