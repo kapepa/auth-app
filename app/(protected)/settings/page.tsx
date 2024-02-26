54220f94b4912059a201f2f41241aba50151045c
kapepa <kapepayx@gmail.com>
2024-02-26 20:59:01 +0200

    change user fields

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -3,8 +3,15 @@
 import { settings } from "@/action/settings";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { FormError } from "@/components/ui/form-error";
+import { FormSuccess } from "@/components/ui/form-success";
+import { Input } from "@/components/ui/input";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { useCurrentUser } from "@/hooks/use-current-user";
 import { SettingsSchema } from "@/schemas";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { UserRole } from "@prisma/client";
 import { NextPage } from "next";
 import { useSession } from "next-auth/react";
 import { useCallback, useState, useTransition } from "react";
@@ -15,11 +22,18 @@ const SettingsPage: NextPage = () => {
   const [error, setError] = useState<string | undefined>();
   const [success, setSuccess] = useState<string | undefined>();
   const [isPending, startTransition] = useTransition();
+  
   const { update } = useSession();
+  const user = useCurrentUser();
+  
   const form = useForm<z.infer<typeof SettingsSchema>>({
     resolver: zodResolver(SettingsSchema),
     defaultValues: {
-      name: "",
+      name: user?.name || "",
+      email: user?.email || "",
+      role: user?.role || undefined,
+      password: undefined,
+      newPassword: undefined,
     }
   })
 
@@ -30,9 +44,10 @@ const SettingsPage: NextPage = () => {
           if(!!data.error) setError(data.error);
           if(!!data.success) setSuccess(data.success);
         })
+        .catch((e) => setError("Something went wrong!"))
         .finally(() => update());
     });
-  }, [])
+  }, [form])
 
   return (
     <Card className="bg-white w-[600px]">
@@ -42,10 +57,129 @@ const SettingsPage: NextPage = () => {
         </p>
       </CardHeader>
       <CardContent>
-        <Button
-          onClick={onClick}
-          disabled={isPending}
-        >Update name</Button>
+        <Form {...form}>
+          <form 
+            className="space-y-6"
+            onSubmit={form.handleSubmit(onSubmit)}
+          >
+            <div className="space-y-4 p-r">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input 
+                        type="text"
+                        placeholder="Your name"
+                        disabled={isPending}
+                        {...field} 
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      This is your public display name.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input 
+                        type="email"
+                        placeholder="email@example.com"
+                        disabled={isPending}
+                        {...field} 
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      This is your email.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>New password</FormLabel>
+                    <FormControl>
+                      <Input 
+                        type="password"
+                        placeholder="******"
+                        disabled={isPending}
+                        {...field} 
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      This is your new password.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="newPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Confirme new password</FormLabel>
+                    <FormControl>
+                      <Input 
+                        type="password"
+                        placeholder="******"
+                        disabled={isPending}
+                        {...field} 
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      Confirme your new password.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="role"
+                render={({ field }) => (
+                  <FormItem>
+                    <Select
+                      disabled={isPending}
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select a role" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent className="z-10">
+                        <SelectItem value={UserRole.ADMIN}>{UserRole.ADMIN}</SelectItem>
+                        <SelectItem value={UserRole.USER}>{UserRole.USER}</SelectItem>
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <FormSuccess message={success} />
+            <FormError message={error} />
+            <Button
+              type="submit"
+              disabled={isPending}
+            >Update</Button>
+          </form>
+        </Form>
       </CardContent>
     </Card>
   )
